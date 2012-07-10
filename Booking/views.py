bb42d3303feadba03d4fea2a62cb80be97788b59
Kiel Byrne <kiel.byrne@gmail.com>
2012-07-09 23:56:50 -0400

    Flights template and view

--- Booking/views.py
+++ Booking/views.py
@@ -6,14 +6,15 @@ import datetime
 
 def booking(request):
 	return render_to_response('booking.html')
-	
+
 def bandRegister(request):
 	if request.method=='POST':
 		form=BandRegisterForm(request.POST)
 		if form.is_valid():
-		return HttpResponseRedirect('/registered/')
-	else: 
-		form=BandRegisterForm()
+			return HttpResponseRedirect('/registered/')
+		else: 
+			form=BandRegisterForm()
 	return render(request, 'contact.html', {
 		'form': form,
 		})
+		
