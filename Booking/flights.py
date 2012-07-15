9bcae63d5cbcd7b780e1fcd4097ecab83980b760
Kiel Byrne <kiel.byrne@gmail.com>
2012-07-14 23:11:20 -0400

    solved empty lists

--- Booking/flights.py
+++ Booking/flights.py
@@ -6,14 +6,26 @@ import pycurl
 import cStringIO
 
 from bs4 import BeautifulSoup as BS
+from bs4 import Tag
 
 from django.shortcuts import render_to_response
 html_doc='none'
 
 def get_html():
 	zURL = 'https://caribbean.sita.aero/itd/itd/DoAirSearch'
-	zARGS='_originSelected=Airport.JFK&_destinationSelected=Airport.POS&_tripType=Return&_depdateeu=08/02/2013%20&_retdateeu=18/02/2013%20&_classType=Economy&_dateWindow=P3D&_searchType=DatePriceLed&requestor=AirSimpleReqsPage&_channelLocale=en&_children=0&_adults=1&_infants=0&_seniors=0'
-
+	zARGS='_originSelected=Airport.JFK&'
+	zARGS+='_destinationSelected=Airport.POS&'
+	zARGS+='_tripType=Return&'
+	zARGS+='_depdateeu=08/02/2013%20&'
+	zARGS+='_retdateeu=18/02/2013%20&'
+	zARGS+='_classType=Economy&_dateWindow=P3D&'
+	zARGS+='_searchType=DatePriceLed&'
+	zARGS+='requestor=AirSimpleReqsPage&'
+	zARGS+='_channelLocale=en&'
+	zARGS+='_children=0&'
+	zARGS+='_adults=1&'
+	zARGS+='_infants=0&'
+	zARGS+='_seniors=0'
 	buf = cStringIO.StringIO()
 	c = pycurl.Curl()
 	c.setopt(c.URL, zURL)
@@ -25,23 +37,23 @@ def get_html():
 	buf.close()
 
 def get_ca_price():
+    global days, dates, prices, title, ca_html
     ca_html = BS(html_doc)
     title = ca_html.title
-    global days, dates, prices
-    days = ca_html.select(".caldow")
-    dates = ca_html.select(".caldate")
-    prices = ca_html.select(".calprice")
+    days = ca_html.select(".headerrow")
+    infos = ca_html.select(".datarow")
+    Tag.do_not_call_in_templates = True
 
 #if __name__ == '__main__':
-#    if len(sys.argv) != 2:
-#        raise Exception("Incorrect number of arguments")
-#    script, filename = sys.argv
-#    if urlparse(filename).netloc:
-#        f = urllib.urlopen(filename)
-#    else:
-#        f = open(filename, 'r')
+# if len(sys.argv) != 2:
+# raise Exception("Incorrect number of arguments")
+# script, filename = sys.argv
+# if urlparse(filename).netloc:
+# f = urllib.urlopen(filename)
+# else:
+# f = open(filename, 'r')
 
 def flights(request):
 	get_html()
 	get_ca_price()
-	return render_to_response('flights.html',{'days':days, 'dates':dates, 'prices':prices})
+	return render_to_response('flights.html',{'days':days,'infos':infos})
